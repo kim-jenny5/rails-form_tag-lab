00f067667a27c2d3f532a755529d71d926e8ffa4
Jenny Kim <kimj1905@gmail.com>
2021-06-28 19:41:29 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,13 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    # redirect_to new_student_path
-    # session[:student] = params.inspect
-    # student[:first_name] = params[:student][:first_name]
-    # student[:last_name] = params[:student][:last_name]
-    # redirect_to new_student_path
-    redirect_to students_path
+    student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(student)
   end
 
 end
